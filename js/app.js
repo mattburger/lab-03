563d3cac9608e678b439a703a3c56f3e21ddf8fd
Matt Burger <matt.burger1@outlook.com>
2019-04-25 09:31:00 -0700

    Setup and carryover from lab-02

---
+++ js/app.js
@@ -0,0 +1,83 @@
+'use strict'
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+
+  $.get('../data/page-1.json', (data, status) => {
+    let imageObjects = [];
+    data.forEach((imgObj) => {
+      imageObjects.push(new ImageObject(imgObj.description, imgObj.horns, imgObj.image_url, imgObj.keyword, imgObj.title))
+    })
+    displayImages(imageObjects);
+    populateKeywords(imageObjects);
+    submitHandler(imageObjects);
+    showAllHandler(imageObjects);
+  })
+})
+
+// constructor function
+function ImageObject(description, horns, imgUrl, keyword, title){
+  this.description = description,
+  this.horns = horns, 
+  this.imgUrl = imgUrl,
+  this.keyword = keyword,
+  this.title = title
+};
+
+var filterPictures = (arr, keyword) => {
+  let usrSelect = [];
+  $.each(arr, function(index, value){
+    if(value.keyword === keyword){
+      usrSelect.push(value);
+    }
+  });
+  removeOptions();
+  displayImages(usrSelect);
+
+}
+// display images function
+const displayImages = (array) => {
+  array.forEach((img) => {
+    $('div.container').append(`<img src="${img.imgUrl}" class="pictures" />`)
+  })
+}
+
+
+// function to remove all option elements
+var removeOptions = () => {
+  $('div.container').empty();
+}
+
+// function to add the filtered keywords
+var populateKeywords = (array) => {
+  let keywords = []
+  $.each(array, (i, obj) => {
+    if(!keywords.includes(obj.keyword)){
+      keywords.push(obj.keyword)
+      $('#keywords').append(`<option value="${obj.keyword}">${firstLetterCap(obj.keyword)}</option>`)
+    }
+  })
+}
+
+// submit handler
+var submitHandler = (array) => {
+  $('#filter-form').submit(function(e) {
+    e.preventDefault();
+    var val = $("#keywords option:selected").val();
+    filterPictures(array, val)
+  });
+}
+
+// show all handler
+var showAllHandler = (array) => {
+  $('#showAll').submit(function(e) {
+    e.preventDefault();
+    removeOptions();
+    displayImages(array)
+  });
+}
+
+const firstLetterCap = (text) => {
+  return text.charAt(0).toUpperCase() + text.slice(1);
+};
+
